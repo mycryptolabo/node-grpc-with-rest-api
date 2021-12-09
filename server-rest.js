fad8a49e4ce17bb2be9df03f03392df3e8987e26
toan.trinh <toan.trinh@ntq-solution.com.vn>
2021-12-09 15:53:28 +0700

    rest-intergrate-grpc-demo

--- server-rest.js
+++ server-rest.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
+const client = require("./client");
+
+// parse an HTML body into a string
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ xtended: true }));
 
 var user = {
   user4: {
@@ -14,6 +20,7 @@ var user = {
 app.get("/users", function (req, res) {
   fs.readFile(__dirname + "/db/" + "users.json", "utf8", function (err, data) {
     console.log(data);
+    res.setHeader("Content-Type", "application/json");
     res.end(data);
   });
 });
@@ -23,7 +30,9 @@ app.get("user/:id", function (req, res) {
   fs.readFile(__dirname + "/db/" + "users.json", "utf8", function (err, data) {
     var users = JSON.parse(data);
     var user = users["user" + req.params.id];
+
     console.log(user);
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(user));
   });
 });
@@ -41,6 +50,7 @@ app.post("/user/create", function (req, res) {
       console.log("The file was saved!");
     });
 
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(data));
   });
 });
@@ -50,7 +60,7 @@ app.delete("/user/:id/delete", function (req, res) {
   fs.readFile(__dirname + "/db/" + "users.json", "utf8", function (err, data) {
     data = JSON.parse(data);
     delete data["user" + req.params.id];
-    
+
     fs.writeFile(__dirname + "/db/" + "users.json", JSON.stringify(data), function (err) {
       if (err) {
         return console.log(err);
@@ -58,7 +68,79 @@ app.delete("/user/:id/delete", function (req, res) {
       console.log("The file was saved!");
     });
 
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(data));
+  });
+});
+
+// get all news
+app.get("/news", function (req, res) {
+  client.getAllNews({}, (error, news) => {
+    if (error) throw error;
+
+    console.log(news);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(news));
+  });
+});
+
+// get news
+app.get("/news/:id", function (req, res) {
+  client.getNews({ id: req.params.id }, (error, news) => {
+    if (error) throw error;
+
+    console.log("News feched successfully!\n", news);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(news));
+  });
+});
+
+// add a news
+app.post("/news/create", function (req, res) {
+  client.addNews(
+    {
+      id: req.body.id,
+      title: req.body.title,
+      body: req.body.body,
+      postImage: req.body.postImage,
+    },
+    (error, news) => {
+      if (error) throw error;
+
+      res.setHeader("Content-Type", "application/json");
+      console.log("Successfully created a news.");
+      res.end(JSON.stringify(news));
+    }
+  );
+});
+
+// edit a news
+app.post("/news/:id/edit", function (req, res) {
+  client.editNews(
+    {
+      id: req.params.id,
+      title: req.body.title,
+      body: req.body.body,
+      postImage: req.body.postImage,
+    },
+    (error, news) => {
+      if (error) throw error;
+
+      console.log("Successfully edited a news.");
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(news));
+    }
+  );
+});
+
+// delete a news
+app.delete("/news/:id/delete", function (req, res) {
+  client.deleteNews({ id: req.params.id }, (error, news) => {
+    if (error) throw error;
+
+    console.log("Successfully deleted a news item.");
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(news));
   });
 });
 
